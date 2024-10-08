e6b2ce21b960ace413574f7daf5a1ba06fd0f41b
thiagaonees <thiagoalves.devp@gmail.com>
2024-10-08 20:59:55 -0300

    :construction: create proect migration, factories and seeds

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,22 +23,13 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'rating' => fake()->randomElement([1,2,3,4,5]),
+            'avatar' => 'https://avatar.iran.liara.run/public'
         ];
     }
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
-    }
 }
