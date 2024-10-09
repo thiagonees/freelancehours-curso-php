190f05e0f5cdba1946f5ed85c2f8b1432fd4cf80
thiagonees <thiagoalves.devp@gmail.com>
2024-10-09 09:57:07 -0300

    :construction: create proposal migration, model and factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Project;
+use App\Models\Proposal;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -17,8 +18,12 @@ class DatabaseSeeder extends Seeder
         User::factory()->count(200)->create();
 
         User::query()->inRandomOrder()->limit(10)->get()
-        ->each(fn(User $u) => Project::factory()->create(
-            ['created_by' => $u->id])
-        );
+        ->each(function (User $u) { 
+           $project = Project::factory()->create(['created_by' => $u->id]);
+
+           Proposal::factory()->count(random_int(4, 45))->create(['project_id' => $project->id]);
+
+
+        });
     }
 }
